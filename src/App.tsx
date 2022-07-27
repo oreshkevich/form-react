14e8b745b464d29d2e4dfb8d93ae21ac39d3d661
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-07-27 18:40:16 +0300

    feat: add form

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Form } from './components/Form';
+import './app.sass';
+
+function App() {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
+
